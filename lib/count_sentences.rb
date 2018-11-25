dfa8054103452dc7d931246783d3b40f5d28e19f
Cheng Vue <cvu3@ucsc.edu>
2018-11-25 17:00:32 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,14 +15,5 @@ class String
   end
 
   def count_sentences
-    counter = 0
-    self.each_char.with_index do |char, idx|
-      if char == "." || char == "?" || char == "!"
-        if char[idx - 1] != char && idx != 0
-          counter += 1
-        end
-      end
-    end
-    counter
-  end
+    self.scan(/\.\s|\?\s|\!\s|\.\z|\?\z|\!\z/).size
 end
